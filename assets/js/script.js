c230589fb6a3af765a7cdf48406e3d3fa11a0f8f
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2022-09-24 17:33:21 +0000

    Adding JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 //Wait for the DOM to finish loading before running the game
 //Get the button elements and add event listeners to them
 
-document.addEventListener ("DOMContentLoaded", function() {
-let buttons = documentGetElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener ("click", function() {
-            if(this.getAttribute("data-type") === "submit") {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
@@ -14,9 +14,18 @@ let buttons = documentGetElementsByTagName("button");
             }
         });
     }
+});
+/**
+ * The main game "loop" called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame () {
+// Creates 2 random numbers in cod between 1 and 25
+let num1=Math.floor(Math.random()*25)+1;
+let num2=Math.floor(Math.random()*25)+1;
 }
 
-function runGame () {}
+runGame();
 
 function checkAnswer () {
 }
