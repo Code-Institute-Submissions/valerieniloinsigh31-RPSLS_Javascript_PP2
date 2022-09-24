0d58311f9123a7a70e60e49db623539092a6b11a
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2022-09-24 17:12:50 +0000

    Adding JS styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,22 @@
 //Wait for the DOM to finish loading before running the game
 //Get the button elements and add event listeners to them
 
-document.addEventListener ("DOMContentLoaded", function() {});
-
-
-function runGame () {
-    let buttons = documentGetElementsByTagName("button");
+document.addEventListener ("DOMContentLoaded", function() {
+let buttons = documentGetElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener ("click", function() {
             if(this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-            }
-            else {
+            } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
         });
-
     }
-
 }
+
+function runGame () {}
 
 function checkAnswer () {
 }
