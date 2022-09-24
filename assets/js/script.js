7ca013f4694e70908cb9103441c0400d45cf2560
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2022-09-24 21:26:30 +0000

    Refining JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ if (gameType === "addition") {
 }
 else {
     alert(`Unknown game type: ${gameType}`);
-    throw `Unknown game type:${gameType}. Aborting`
+    throw `Unknown game type: ${gameType}. Aborting`
 }
 }
 
@@ -47,9 +47,11 @@ function checkAnswer () {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if(isCorrect) {
-        alert("Hey! You got it right! :D")
+        alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert (`Awww...you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -72,13 +74,20 @@ if (operator === "+") {
     throw `Unimplemented operator ${operator}. Aborting!`;
 }
 }
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 
 function incrementScore () {
-
+let oldScore= parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText= ++oldScore;
 }
-
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer () {
-
+    let oldScore= parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText= ++oldScore;
 }
 
 function displayAdditionQuestion (operand1, operand2) {
