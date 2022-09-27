b3cfa5f873ee28a66781ffa51c3b3f923c67b4dd
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2022-09-27 16:00:38 +0000

    Commenting out code deemed unnecessary. Trying to make the buttons functional.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,11 @@
  * and possible choices. Because these are global scope,
  * will apply to the full page of Javascript
  */
-const buttons= document.getElementById("controls");
+const buttons= document.getElementById("controls");//buttons activated and used to retrieve data-choice
 const playerScore = document.getElementById("player-score");//variable used in scoring system at end
 const computerScore = document.getElementById("computer-score");//variable used in scoring system at end
+playerScore=0;//this will be increment 
+computerScore=0;//this will be incremeted
 
 /**The player-image and computer-image in scope version of game change based on selection of button (e.g. whether rock, paper, scissors, lizard or spock was selected)
  * For each of the images, what needs to be done is the image will update based on what playerChoice and computerChoices were
@@ -24,9 +26,11 @@ const computerScore = document.getElementById("computer-score");//variable used 
  * playerChoice and computerChoice
  */
 
-const playerImage = document.getElementById("player-image");//this will change based on playerChoice so filepaths need to be 
+const playerImage = document.getElementById("player-image");
+//this will change based on playerChoice so filepaths need to be 
 //named correctly
-const computerImage = document.getElementById("computer-image");//this will change based on computerChoice so filepaths need
+const computerImage = document.getElementById("computer-image");
+//this will change based on computerChoice so filepaths need
 //to be named correctly
 
 /**Message prompted is different based on whether the player or the computer wins-maybe use just one message once best of three 
@@ -57,7 +61,7 @@ const choices = ["rock", "paper", "scissors", "lizard", "spock"]
                 runGame(playerChoice);
             }
         });
-    
+
 /**
  * The main game function, which is a parameter used in the above playerChoice function.
  * Accepts one parameter, which
@@ -74,7 +78,7 @@ const choices = ["rock", "paper", "scissors", "lizard", "spock"]
 function runGame(playerChoice) {
 
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt= choices[playerchoice];
+    playerImage.alt= choices[playerChoice];
 
 /**
  * Defining the computer choice to be randomly generated-5 choices to choose form in the array (0,1,2,3,4)
@@ -82,22 +86,24 @@ function runGame(playerChoice) {
  * Additionally, as per player choice, the correct corresponding image should be triggered once selection is made
  * Explain why I have used a switch
  */
-let computerChoice= Math.floor(Math.random()*5)+1;
-switch(computerChoice){
-    case 1: computerChoice = "rock";
-    break;
-    case 2 : computerChoice = "paper";
-    break;
-    case 3 : computerChoice = "scissors";
-    break;
-    case 4 : computerChoice = "lizard";
-    break;
-    case 5 : computerChoice = "spock";
-    break;}
+let computerChoice= Math.floor(Math.random() * 5);
+
+//commented out code, could also use a switch instead of math and array and index
+//switch(computerChoice){
+    //case 1: computerChoice = "rock";
+    //break;
+    //case 2 : computerChoice = "paper";
+    //break;
+    //case 3 : computerChoice = "scissors";
+    //break;
+    //case 4 : computerChoice = "lizard";
+    //break;
+    //case 5 : computerChoice = "spock";
+    //break;}
 
 
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
-    computerImage.alt= choices[computerchoice];
+    computerImage.alt= choices[computerChoice];
 
     let result= compare(choices[computerChoice], choices[playerChoice]);
 }
@@ -129,58 +135,60 @@ switch(computerChoice){
 //for the choices, take it one by one and have an else if for each of the alternatives in each case
 ///**
 
+//for below function, is it sufficient to say computerChoice...will that be word format...should parseInt be used
+
 function compare(playerChoice, computerChoice){
    if (playerChoice === computerChoice) {
-       answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>The result is a Tie!`
+       answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>You are tied!`
    } else if (playerChoice === 'rock') {
        if (computerChoice === 'paper'){
-           answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br> Paper beats Rock. You lose!`
+           answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br> Paper beats Rock. You lose this time!`
        } else if (computerChoice === 'scissors'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Scissors. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Scissors. You win this time!`
        } else if (computerChoice === 'lizard'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Lizard. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Lizard. You win this time!`
        } else {
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Rock. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Rock. You lose this time!`
        }
    } else if (playerChoice === 'paper') {
        if (computerChoice === 'scissors'){
-         answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Scissors cuts Paper. You lose!`
+         answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Scissors beats Paper. You lose this time!`
        } else if (computerChoice === 'lizard'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Lizard eats Paper. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Lizard beats Paper. You lose this time!`
        } else if (computerChoice === 'spock'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper disproves Spock. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper beats Spock. You win this time!`
        } else {
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper covers Rock. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper beats Rock. You win this time!`
        }
    } else if (playerChoice === 'scissors') {
        if (computerChoice === 'rock'){
-            answer = `You chose: ${playerchoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Scissors. You lose!`
+            answer = `You chose: ${playerchoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Scissors. You lose this time!`
        } else if (computerChoice === 'paper'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Scissors beats Paper. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Scissors beats Paper. You win this time!`
        } else if (computerChoice === 'lizard'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Scissors beats Lizard. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Scissors beats Lizard. You win this time!`
        } else {
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Scissors. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Scissors. You lose this time!`
        }
    } else if (playerChoice === 'lizard') {
        if (computerChoice === 'rock'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Scissors. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Scissors. You lose this time!`
        } else if (computerChoice === 'paper'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Lizard beats Paper. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Lizard beats Paper. You win this time!`
        } else if (computerChoice === 'spock'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Lizard beats Spock. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Lizard beats Spock. You win this time!`
        } else {
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Lizard. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Rock beats Lizard. You lose this time!`
        }
    }  else if (playerChoice === 'spock') {
        if (computerChoice === 'rock'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Rock. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Rock. You win this time!`
        } else if (computerChoice === 'paper'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper beats Spock. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper beats Spock. You lose this time!`
        } else if (computerChoice === 'scissors'){
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Scissors. You win!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Spock beats Scissors. You win this time!`
        } else {
-            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper beatss Spock. You lose!`
+            answer = `You chose: ${playerChoice}<br>The Lizard chose: ${computerChoice}<br>Paper beatss Spock. You lose this time!`
        }
    }
    result (answer);
@@ -195,6 +203,7 @@ function compare(playerChoice, computerChoice){
  * This code is based on the contents of the template literal options within the previous compare function code
  * If the resulting answer includes 'win', then the playerScore is increased in increments of 1
  * If the resulting answer includes 'lose', the Lizard's score increased in increments of 1
+ * the inner HTML & template literal will update the '0' currently in the tex space
  */
 
  function incrementScore() {
@@ -206,11 +215,26 @@ function compare(playerChoice, computerChoice){
     } else {
         console.log('0')
     }
-    document.getElementById("user-score").innerHTML = `Your Score:<br>${playerScore}`;
-    document.getElementById("comp-score").innerHTML = `Lizard's Score:<br>${computerScore}`;
+    document.getElementById("player_score").innerHTML = `Your Score:<br>${playerScore}`;
+    document.getElementById("computer_score").innerHTML = `Lizard's Score:<br>${computerScore}`;
     
     checkScore();
 }
+
+/**
+ * Checks the score and declares game over when threshold has been met.
+ * Applies best of 3 logic
+ */
+ function checkScore () {
+    if (playerScore === 3) {
+        document.getElementById("welcome").innerHTML = "Well done! <br> You have won! <br>";
+       
+    } else if (computerScore === 3) {
+        document.getElementById("welcome").innerHTML = "Game Over! <br> You have lost! <br>";
+    }
+}
+
+
 
 /**
  * Message Pop-Ups. Need to define 'playerWinner' if user wins best of three to follow along with the rules of the game. There are a number of
@@ -238,19 +262,19 @@ function compare(playerChoice, computerChoice){
  * Gets the current score from the DOM and increments it by 1-need to define 'incrementScore at some point
  */
 
- function incrementScore () {
-    let oldScore= parseInt(document.getElementById("player_score").innerText);
-    document.getElementById("player_score").innerText= ++oldScore;
-    }
+ //function incrementScore () {
+    //let oldScore= parseInt(document.getElementById("player_score").innerText);
+    //document.getElementById("player_score").innerText= ++oldScore;
+    //}
     /**
      * Gets the current tally of times the Lizard won the game of RPSLS from the DOM and increments it by 1. Need to define 
      * incrementWrongChoice at some point. Need to call these functions at appropriate times, e.g. if user has selected an options
      * that destroys computer/Lizard's option, incrementScore will be updated. If computer/Lizard's option destorys user's
      * option, incrementWrongChoice will be called
      */
-    function incrementWrongChoice () {
-        let oldScore= parseInt(document.getElementById("computer_score").innerText);
-        document.getElementById("computer_score").innerText= ++oldScore};
+    //function incrementWrongChoice () {
+        //let oldScore= parseInt(document.getElementById("computer_score").innerText);
+        //document.getElementById("computer_score").innerText= ++oldScore};
 
 /**
  * Empty functions that I think I need
